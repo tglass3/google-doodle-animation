595cb77646d092c56e0cf8e1f623ca5c1655f152
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-06-08 22:23:13 +0000

    Random Number Generator works

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log('OK');
+console.log('ready to go!');
+
+let swordPull = Math.floor(Math.random() * 100) + 1;
+
+const doodle = document.querySelector('#stage');
+
+
+stage.addEventListener('click', item => {
+if (swordPull == 42) {
+    console.log(swordPull);
+    
+    gsap.fromTo
+
+    swordPull = Math.floor(Math.random() * 100) + 1;
+}
+
+else {
+    console.log(swordPull)
+    console.log('You are unworthy to pull the sword from the stone.');
+    swordPull = Math.floor(Math.random() * 100) + 1;
+}
+
+});
