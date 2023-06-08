542f8943da88c5b0a4609ad5f89ee15e3b2fa60f
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-06-08 23:33:05 +0000

    Finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
+
 console.log('ready to go!');
 
-// let swordPull = Math.floor(Math.random() * 100) + 1;
+let swordPull = Math.floor(Math.random() * 50) + 1;
 
-let swordPull = 42;
+// let swordPull = 40;
+
 
 const doodle = document.querySelector('#stage');
 
 
+
 stage.addEventListener('click', item => {
-if (swordPull == 42) {
+if (swordPull == 25) {
     console.log(swordPull);
     
-    gsap.to('#sword', {duration: 1, x: 0, y: -65}, yoyo);
+    gsap.to('#sword', {duration: 1, x: 0, y: -65, yoyo: 'true', repeat: 1, repeatDelay: 2});
 
-    swordPull = Math.floor(Math.random() * 100) + 1;
+    swordPull = Math.floor(Math.random() * 50) + 1;
 }
 
 else {
     console.log(swordPull)
 
-    gsap.to('#sword', {duration: 1, x: 0, y: 0});
+    // gsap.to('#sword', {duration: .075, x:-2, yoyo: 'true', repeat: 1});
+    gsap.to('#sword', 0.1, {x:"2", yoyo:'true', repeat:3});
+    gsap.to('#sword', 0.1, {x:"-2", yoyo:'true', repeat:3});
     
-    swordPull = Math.floor(Math.random() * 100) + 1;
+    
+    swordPull = Math.floor(Math.random() * 50) + 1;
 }
 
 });
